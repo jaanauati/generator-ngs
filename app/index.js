2c771977e046ab6620c2783a9a8320904a718437
Jonatan Alexis Anauati <jaanauati@gmail.com>
2014-09-06 02:07:34 -0300

    DEFAULT CONFIGURATION AND STORAGE.
    - Put defaults settings into config.json (choices and directories)
    - Use storage api to generate .yo-rc.json (which stores user selected
      settings at generation time).

--- app/index.js
+++ app/index.js
@@ -15,6 +15,11 @@ var NgsGenerator = module.exports = function NgsGenerator(args, options) {
     });
 
     this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+
+    // Loads generator settings.
+    this.defaultConfig = JSON.parse(
+        this.readFileAsString(path.join(__dirname, 'config.json'))
+    );
 };
 
 util.inherits(NgsGenerator, yeoman.generators.Base);
@@ -32,64 +37,66 @@ NgsGenerator.prototype.askFor = function askFor() {
         type: 'confirm',
         name: 'useBackbone',
         message: 'Would you like to include Backbone.js?',
-        default: true
+        default: this.defaultConfig.useBackbone
     }, {
         type: 'confirm',
         name: 'useRequire',
         message: 'Would you like to include RequireJS (for AMD support)?',
-        default: true
+        default: this.defaultConfig.useRequire
     }];
 
     this.prompt(prompts, function (props) {
-        this.projectName = props.projectName;
-
-        this.useBackbone = props.useBackbone;
-        this.useRequire = props.useRequire;
-
+        for (var propName in props) {
+            this.config.set(propName, props[propName]);
+        }
         cb();
     }.bind(this));
 };
 
 NgsGenerator.prototype.askForPreprocessors = function askForPreprocessors() {
     var cb = this.async();
+
     var prompts = [{
         name: 'cssPre',
         type: 'list',
         message: 'Will you be using a CSS preprocessor?',
-        choices: ['None', 'Compass']
+        choices: this.defaultConfig.cssPreChoices,
+        default: this.defaultConfig.cssPreDefChoice
     }, {
         name: 'autoPre',
         type: 'confirm',
-        message: 'Use the Autoprefixer for your CSS?'
+        message: 'Use the Autoprefixer for your CSS?',
+        default: this.defaultConfig.autoPre
     }, {
         name: 'jsPre',
         type: 'list',
         message: 'How about a Javascript preprocessor? We like Coffeescript.',
-        choices: ['None', 'Coffeescript']
+        choices: this.defaultConfig.jsPreChoices,
+        default: this.defaultConfig.jsPreDefChoice
     }];
 
     this.prompt(prompts, function (props) {
-        this.cssPre  = props.cssPre === 'None' ? false : props.cssPre.toLowerCase();
-        this.autoPre = props.autoPre;
-        this.jsPre   = props.jsPre === 'None' ? false : props.jsPre.toLowerCase();
-
+        for (var propName in props) {
+            this.config.set(propName, props[propName]);
+        }
         cb();
     }.bind(this));
 };
 
 // Begin Generating App/UI
 NgsGenerator.prototype.app = function app() {
-    this.mkdir('templates');
-    this.mkdir('js');
-    this.mkdir('css');
+    this.context = this.config.getAll();
+    this.mkdir(this.defaultConfig.templatesDir);
+    this.mkdir(this.defaultConfig.javascriptsDir);
+    this.mkdir(this.defaultConfig.stylesheetsDir);
 
     this.template('_package.json', 'package.json');
     this.template('_bower.json', 'bower.json');
 };
 
 NgsGenerator.prototype.preprocessors = function preprocessors() {
-    if (this.cssPre) { this.mkdir('scss'); }
-    if (this.jsPre) { this.mkdir('coffee'); }
+    if (this.config.get('cssPre')) { this.mkdir(this.defaultConfig.scssDir); }
+    if (this.config.get('jsPre')) { this.mkdir(this.defaultConfig.coffeescriptDir); }
 };
 
 NgsGenerator.prototype.git = function git() {
@@ -111,21 +118,21 @@ NgsGenerator.prototype.gruntCleanup = function gruntCleanup() {
     // We're going to cleanup any tasks not needed
     var cb = this.async();
 
-    if (!this.cssPre) {
+    if (!this.config.get('cssPre')) {
         // this.copy('_grunt/compass.coffee', 'grunt/compass.coffee');
         rimraf('grunt/compass.coffee', function () {
             console.log('Removing Compass task');
         });
     }
 
-    if (!this.autoPre) {
+    if (!this.config.get('autoPre')) {
         // this.copy('_grunt/compass.coffee', 'grunt/compass.coffee');
         rimraf('grunt/autoprefixer.coffee', function () {
             console.log('Removing Autoprefixer task');
         });
     }
 
-    if (!this.jsPre) {
+    if (!this.config.get('jsPre')) {
         rimraf('grunt/coffeelint.coffee', function () {
             console.log('Removing Coffeelint task');
         });
